6c82063c2580bd6e7794d08736baab1c89ee10d8
shashankyo <114981861+shashankyo@users.noreply.github.com>
2023-02-09 07:20:39 +0530

    Add files via upload
    inserting by user input

--- Program.java
+++ Program.java
@@ -1,6 +1,7 @@
-package insertingintotable;
+package insertingtotableusinguserinput;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Program {
 public static void main(String[] args) {
@@ -8,22 +9,29 @@ public static void main(String[] args) {
 	String url = "jdbc:oracle:thin:@localhost:1521:xe";
 	String un = "system";
 	String pw = "system";
-	Connection con = null;
-	Statement stmt = null;
-	// Step 4: creating a query
-	String query = "insert into car values('Audi',2000000)";
+	Connection con  = null;
+	PreparedStatement pstmt = null;
+	//Step4: Creating incomplete query
+	String query = "insert into car values(?,?)";
 	try
 	{
-		//Step1: Loading the Driver
+		//Step1 : Loading the Driver
 		Class.forName(path);
 		System.out.println("Driver is loaded successfully");
-		//Step2: Establishing the connection
+		//Step2 :Establishing the connection
 		con = DriverManager.getConnection(url,un,pw);
 		System.out.println("Connection is established successfully");
-		//Step3 : Creating  statement
-		stmt = con.createStatement();
-		//Step5 : Executing the Query
-		stmt.executeUpdate(query);
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the brand name of car");
+		String brand = scan.next();
+		System.out.println("Enter the cost of car");
+		int cost = scan.nextInt();
+		//Step 3: Creating prepared statement
+		pstmt = con.prepareStatement(query);
+		pstmt.setString(1,brand);
+		pstmt.setInt(2, cost);
+		//Step 5:Executing the query
+		pstmt.executeUpdate();
 		System.out.println("Query has been executed");
 	}
 	catch(Exception e)
